29a9d844ad0da2f9dc9635d876ebbc1cc785a752
Modini Ayyagari <modiniayyagari@gmail.com>
2024-07-23 17:17:02 -0400

    TOOL: Update test.py
    Trial for logfile output messages

--- test/test.py
+++ test/test.py
@@ -34,11 +34,12 @@ async def test_project(dut):
 
     # Wait for one clock cycle to see the output values
     await ClockCycles(dut.clk, 1)
-    dut._log.info("MIAI: Output", dut.uo_out.value)
+    alu_output = dut.uo_out.value
+    dut._log.info("MIAI: ALU for Output AND", alu_output)
 
     # The following assertion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 0x00
+    assert dut.uo_out.value == 0x01
 
     await ClockCycles(dut.clk, 1)
 
